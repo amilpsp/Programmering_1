e3ad02f5fe072f05223b460f1a0b341837a9e8b7
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-04-27 11:23:45 +0200

    Day 6

---
+++ src/P1_class6_KlotAreaOchVolym.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class P1_class6_KlotAreaOchVolym {
+
+    public static void main(String[]args){
+
+        //KlotArea = 4 · pi · r²
+        //KlotVolym = (4 · pi · r³)/3
+
+        Scanner rVarde = new Scanner(System.in);
+        System.out.println("Bestäm värden av klotets radius: ");
+        double r = rVarde.nextDouble();
+
+        double klotArea = 4 * Math.PI * Math.pow( r , 2 );
+        double klotVolym = (4 * Math.PI * Math.pow( r , 3 ) ) / 3;
+
+        int klotAreaRounded = (int)Math.round(klotArea);
+        int klotVolymRounded = (int)Math.round(klotVolym);
+
+        System.out.println("Arean av klotet är: " + klotAreaRounded );
+        System.out.println("Volymen av klotet är: " + klotVolymRounded );
+    }
+}
