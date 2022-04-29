bb1f7250e8d2636fde167e76cedfc703ca85cdf1
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-04-29 15:16:56 +0200

    After class 5: class notes and the 4th uppgift

--- src/P1_inlU_3_forstaOchSistaOrd.java
+++ src/P1_inlU_3_forstaOchSistaOrd.java
@@ -9,16 +9,22 @@ public class P1_inlU_3_forstaOchSistaOrd {
         String tvaOrdMening = scanMening.nextLine();                //Storing that string
         String trimmatMening = tvaOrdMening.trim();                 //Triming in case people add spaces at the beggining or the end
 
-        int length= trimmatMening.length();//".length" method
+        int length= trimmatMening.length();                         //".length" method
         System.out.println("Du skrev " + length + " tecken.");      //Printing on the console
 
         int iOfFirstSpace = trimmatMening.indexOf(" ");             //The index of the first space is the end of the fist word.
 
-        String forstaOrdet = trimmatMening.substring(0,iOfFirstSpace);          //Making a substring from the beginning until the first space.
-        System.out.println("Det första ordet var \"" + forstaOrdet + "\".");    //Printing on the console
+        String forstaOrdet = trimmatMening.substring(
+                0,iOfFirstSpace);                                   //Making a substring from the beginning until the first space.
+        System.out.println(
+                "Det första ordet var \"" + forstaOrdet + "\".");   //Printing on the console
 
-        int indexOfLastSpace = trimmatMening.lastIndexOf(" ");              //The index of the last space is the beginning of the last word.
-        String sistaOrdet = trimmatMening.substring(indexOfLastSpace + 1);//Making a substring from the after the last space.
-        System.out.println("Det sista ordet var \"" + sistaOrdet + "\".");      //Printing on the console
+        int indexOfLastSpace = trimmatMening.lastIndexOf(" ");  //The index of the last space is the beginning of the last word.
+
+        String sistaOrdet = trimmatMening.substring(
+                indexOfLastSpace + 1                                //Making a substring from the after the last space.
+        );
+        System.out.println(
+                "Det sista ordet var \"" + sistaOrdet + "\".");     //Printing on the console
     }
 }
