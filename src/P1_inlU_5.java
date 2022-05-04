81c0c3b7510e4f344e05f6adc709eddf09c04cf6
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-04 19:00:04 +0200

    Examples for uppgift 5

--- src/P1_inlU_5.java
+++ src/P1_inlU_5.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class P1_inlU_5 {
     public static void main(String[]args){
-        int x = 5;
+
+
+    /*
+        int x = scanForX();
         vadArX(x);
+    */ //In what value range is X at?
     }
 
+
+
+    public static int scanForX(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Skriv ett nummer: ");
+        int x = scan.nextInt();
+        return x;
+    }
     public static void vadArX(int x){
         if (x == 10)
             System.out.println("x = 10");
