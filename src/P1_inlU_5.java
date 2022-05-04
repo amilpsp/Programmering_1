3257e273c369e651a5f0297e8cb04a97a6241a88
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-04 21:49:53 +0200

    Examples for uppgift 5

--- src/P1_inlU_5.java
+++ src/P1_inlU_5.java
@@ -3,14 +3,31 @@ import java.util.Scanner;
 public class P1_inlU_5 {
     public static void main(String[]args){
 
+        int k;
+        System.out.print("k =");
 
-    /*
-        int x = scanForX();
-        vadArX(x);
-    */ //In what value range is X at?
+        //   innit  condition  increment
+        for ( k = 0 ; k < 6 ; k += 2){
+            System.out.print(" " + k);
+        }
+
+
+/*        int x = scanForX();
+        varArX(x);
+         //In what value range is X at? */
+
     }
 
+    public static void whileLoop(){
+        int k = 0;
+        String s = "(While loop) k = " ;
 
+        while( k < 6) {
+            s = s + " " + k;
+            k = k + 2;
+        }
+        System.out.print(s);
+    }
 
     public static int scanForX(){
         Scanner scan = new Scanner(System.in);
@@ -18,12 +35,12 @@ public class P1_inlU_5 {
         int x = scan.nextInt();
         return x;
     }
-    public static void vadArX(int x){
+    public static void varArX(int x){
         if (x == 10)
             System.out.println("x = 10");
-        else if (x <= 10 && x >= 0)
+        else if (x < 10 && x > 0)
             System.out.println("x är mindre än 10, men större än 0");
-        else if ( x<=0 && x!=0 )
+        else if ( x < 0 && x != 0 )
             System.out.println("x är negativ");
         else
             System.out.println("x är större än 10");
