5cf232768ec126f0dc08a6076f548d589e99ad95
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-10 11:23:34 +0200

    No longer prints empty space at the end of the line or a new line at the end of the last traversal of the loop.

--- src/inlU6_Ovning5_5.java
+++ src/inlU6_Ovning5_5.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 //SANDER'S COMMENTARY:
 // if you put something in that isn't an integer it crashes
-// extra spaces after the "j"
 // indexes start at 0 in arrays, may be best to rename for clarity.
-
 public class inlU6_Ovning5_5 {
     public static void main(String[]args){
         Scanner antalRaderScan = new Scanner(System.in);
@@ -14,11 +12,18 @@ public class inlU6_Ovning5_5 {
         int i; int j;
 
         for ( i = 1 ; i <= antalRader; i++ ){
-            System.out.print( i + " ");
+            if (i == 1)
+                System.out.print(i);
+            else
+                System.out.print( i + " ");
 
             for ( j = i * 2 ; j <= (i*i) ; j += i ){
-                System.out.print(j + " ");
+                if (j != (i*i) )
+                    System.out.print(j + " ");
+                else
+                    System.out.print(j);
             }
+            if (i != antalRader)
             System.out.println();
         }
     }
