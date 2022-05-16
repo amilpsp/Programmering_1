d8f59af527a478be384e825afb33a70e21b512e2
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-16 19:20:46 +0200

    added Martin's loop to the file

--- src/inlU6_MultiplicationPyramid.java
+++ src/inlU6_MultiplicationPyramid.java
@@ -10,21 +10,35 @@ public class inlU6_MultiplicationPyramid {
         int antalRader = antalRaderScan.nextInt();
 
         int i; int j;
-
-        for ( i = 1 ; i <= antalRader; i++ ){
-            if (i == 1)
-                System.out.print(i);
-            else
-                System.out.print( i + " ");
-
-            for ( j = i * 2 ; j <= (i*i) ; j += i ){
-                if (j != (i*i) )
-                    System.out.print(j + " ");
-                else
-                    System.out.print(j);
-            }
-            if (i != antalRader)
-                System.out.println();
+    /*      2022-05-16
+            Martin showed me this more reader-friendly way of looping this.
+            Although mine could have a slightly better performance,
+            because it relies on addition (simpler and faster operation to do).*/
+        for (i =1; i<=antalRader; i++){
+            for (j =1; j<=i; j++){
+                if ( i * j != (i*i) )
+                        System.out.print(i*j+" ");
+                    else
+                        System.out.print(i*j);
         }
+        System.out.println();
+    }
+    /*   My initial solution (I'm still very proud of it <3)
+
+    for ( i = 1 ; i <= antalRader; i++ ){
+        if (i == 1)
+            System.out.print(i);
+        else
+            System.out.print( i + " ");
+
+        for ( j = i * 2 ; j <= (i*i) ; j += i ){
+            if (j != (i*i) )
+                System.out.print(j + " ");
+            else
+                System.out.print(j);
+        }
+        if (i != antalRader)
+            System.out.println();
+        }*/
     }
 }
