c9d270246d43f6f11ebc7d876ebc4da3d7aa647f
amilpsp <98482837+amilpsp@users.noreply.github.com>
2022-05-18 15:29:23 +0200

    It runs. Shit results, but it runs. XD

--- src/FinalInlU_TaxCalculator.java
+++ src/FinalInlU_TaxCalculator.java
@@ -4,55 +4,62 @@ import java.util.Scanner;
 * Fix if the input breaks with commas instead of points*/
 public class FinalInlU_TaxCalculator {
     public static void   main(String[]args){
-    boolean   calculateAnother   =     true;
+    boolean  calculateAnother    =     true;
     int      stateTaxBracket     =   540700;      /*In the Skatteverket page it said that this year there's
                                                   only one skiktgräns, from where one pays 20%*/
     int      basicDeduction      ;
     int       preTaxIncome       ;
     int   incomeAfterDeduction   ;
     int         taxAmount        ;
-
+    int      firstComunalTax     ;
 
     while (calculateAnother){
         //Method calls for user input.
-        preTaxIncome  = requestIncome();
-        basicDeduction = determineBasicDeduction();
+        preTaxIncome         = requestIncome();
+        basicDeduction       = determineBasicDeduction();
         incomeAfterDeduction = preTaxIncome - basicDeduction;
 
         if (incomeAfterDeduction<=0){
             System.out.println(
-                    "Your yearly income is less than the basic deduction, so you don't have to pay any tax on it whatsoever.");
+                    "Your yearly income is less than the basic deduction, so you don't have to pay any tax on it whatsoever."
+            );
         }
-
-
-        int incomeInStateBracket = incomeAfterDeduction-stateTaxBracket;
-
-        /*
-        while (incomeInStateBracket>0){
-
+        else{
+        if(incomeAfterDeduction<stateTaxBracket){
+            firstComunalTax = calculateMunicipalTax(incomeAfterDeduction);
+            System.out.println(
+                    "Your yearly income is less than the State Tax Bracket, so you don't have to pay any tax to the state.");
+            System.out.println("" +
+                    "Although you do have to pay " + firstComunalTax + " kr in communal tax.");
         }
-        */
+        else {
+            firstComunalTax          = calculateMunicipalTax(stateTaxBracket);
+            int amountInStateBracket = incomeAfterDeduction - stateTaxBracket;
+            int secondComunalTax     = calculateMunicipalTax(amountInStateBracket);
+            int totalComunalTax  = firstComunalTax + secondComunalTax;
+            int stateTax         = calculateStateTax(amountInStateBracket);
+            int totalTax         = totalComunalTax + stateTax;
 
-
-
-
+            System.out.println(
+                    "You must pay a total of " + totalTax + " kr in taxes this year,");
+            System.out.println(
+                    "of which " + totalComunalTax + " kr go to the Kommun, and " +stateTax+ " kr go to the State.");
+        }
+        }
 
         calculateAnother = continueQuestion();
         }//end of while loop
 
     }//end of main
-    public static int calculateStateTax(int incomeInStateBracket, int stateTaxBracket){
-
+    public static int calculateStateTax(int incomeInStateBracket){
         int  stateTaxPercentage     = 20;
-        int stateTaxAmount = (incomeInStateBracket) * stateTaxPercentage / 100;
-        return stateTaxAmount;
+        return incomeInStateBracket * stateTaxPercentage / 100;
     }
     public static int calculateMunicipalTax(int incomeAfterDeduction){
 
         int municipalTaxPercentage = 20;
-        int municipalTaxAmount = incomeAfterDeduction * municipalTaxPercentage / 100;
 
-        return municipalTaxAmount;
+        return incomeAfterDeduction * municipalTaxPercentage / 100;
     }
     public static int requestIncome(){
         Scanner incomeScan =new Scanner(System.in);
